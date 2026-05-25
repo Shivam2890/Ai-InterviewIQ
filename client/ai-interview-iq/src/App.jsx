9a506844e24feb67f2c78895a18bdf68374bc5d4
Shivam2890 <Shivamkori2890@gmail.com>
2026-05-25 16:57:45 +0530

    implemented toastify signup

--- client/ai-interview-iq/src/App.jsx
+++ client/ai-interview-iq/src/App.jsx
@@ -3,10 +3,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Home from './components/Home';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
     <>
+      <ToastContainer
+        // position="top-right"
+        // autoClose={5000}
+        // hideProgressBar={false}
+        // newestOnTop={false}
+        // closeOnClick={false}
+        // rtl={false}
+        // pauseOnFocusLoss
+        // draggable
+        // pauseOnHover
+        // theme="light"
+      // transition={Bounce}
+      />
       <BrowserRouter>
         <Routes>
           <Route path='/signup' element={<Signup />} />
--- client/ai-interview-iq/src/components/Signup.jsx
+++ client/ai-interview-iq/src/components/Signup.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Signup = () => {
     const [formValues, setFormValues] = useState({
@@ -9,32 +11,65 @@ const Signup = () => {
         password: "",
         confirmPassword: ""
     })
+    function updateFormData(e) {
+        const { name, value } = e.target
+
+        const updateFormValues = { ...formValues }
+        updateFormValues[name] = value
+
+        if (name == "phone" && value.length > 10) {
+            console.log('wronf number format')
+            return
+        }
+
+        setFormValues(updateFormValues)
+    }
+    async function signUp(e) {
+        e.preventDefault()
+        if (formValues.password !== formValues.confirmPassword) {
+            toast('🦄 Password does not match', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                // transition: Bounce,
+            });
+            return
+        }
+        const data = await axios.post('http://localhost:4000/auth/signup')
+        console.log(data, 'data form signup')
+    }
+    console.log(formValues, 'form vlaue')
     return (
         <div className='h-screen flex justify-center items-center'>
-            <form className='flex gap-3 flex-col'>
+            <form className='flex gap-3 flex-col' onSubmit={signUp}>
                 <div>
                     <label htmlFor="name">Name</label>
-                    <input className='border' type="text" name="name" id="name" />
+                    <input className='border' type="text" value={formValues.name} name="name" id="name" onChange={updateFormData} />
                 </div>
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input className='border' type="text" name="email" id="email" />
+                    <input className='border' type="text" value={formValues.email} name="email" id="email" onChange={updateFormData} />
                 </div>
                 <div>
                     <label htmlFor="age">Age</label>
-                    <input className='border' type="number" name="age" id="age" />
+                    <input className='border' type="number" value={formValues.age} name="age" id="age" onChange={updateFormData} />
                 </div>
                 <div>
-                    <label htmlFor="phone">Name</label>
-                    <input className='border' type="text" name="phone" id="phone" />
+                    <label htmlFor="phone">Phone</label>
+                    <input className='border' type="text" value={formValues.phone} name="phone" id="phone" onChange={updateFormData} />
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input className='border' type="password" name="password" id="password" />
+                    <input className='border' type="password" value={formValues.password} name="password" id="password" onChange={updateFormData} />
                 </div>
                 <div>
-                    <label htmlFor="confirm-password">Confirm Password</label>
-                    <input className='border' type="password" name="confirm-password" id="confirm-password" />
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input className='border' type="password" value={formValues.confirmPassword} name="confirmPassword" id="confirmPassword" onChange={updateFormData} />
                 </div>
 
                 <div>
